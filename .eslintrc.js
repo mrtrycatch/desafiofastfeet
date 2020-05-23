d24b47a6d0fdfbae0e831689a920412d4d13573c
Vinicius de Carvalho Barboza <vini_000@live.com>
2020-05-23 13:41:46 -0300

    Primeiro commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-method-use-this": "off",
+    "no-param-ressign": "off",
+    "camel-case": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
