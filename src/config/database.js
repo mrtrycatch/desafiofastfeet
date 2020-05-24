44949d780e40322509367164256f28f5cc42ecba
Vinicius de Carvalho Barboza <vini_000@live.com>
2020-05-24 00:55:48 -0300

    Migration recipients e seed users

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: "postgres",
-  host: "192.168.99.10",
-  username: "postgres",
-  password: "docker",
-  database: "fastfeet",
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'docker',
+  database: 'fastfeet',
   define: {
     timestamps: true,
     underscored: true,
---
+++ src/database/migration/20200524022353-create-recipients.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoImcrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rua: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      complemento: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      estado: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cidade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('recipients');
+  },
+};
