e97f4e121ac7a959c3b62b9096767836d4df797e
Vinicius de Carvalho Barboza <vini_000@live.com>
2020-05-26 00:55:56 -0300

    autenticação do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,10 +8,23 @@ class User extends Model {
         nome: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         provider: Sequelize.BOOLEAN,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,12 @@
 import { Router } from 'express';
-import Recipient from './app/models/recipient';
+//import Recipient from './app/models/recipient';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const recipient = await Recipient.create({
-    nome: 'Vinicius',
-    rua: 'São Domingos da Calçada',
-    numero: 277,
-    estado: 'RJ',
-    cidade: 'Resende',
-    cep: '27535020',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(recipient);
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
